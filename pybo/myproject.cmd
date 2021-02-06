ad14acd0a9e5d84c5455e35e4e85506d4f8869ae
daelim <blackway76@gmail.com>
2021-02-06 13:13:04 +0900

    2-03 블루프린트로 라우트 함수 관리하기

---
+++ pybo/myproject.cmd
@@ -0,0 +1,7 @@
+@echo off
+cd C:/dev/works/python_web_framework/flask_works/jump_to_flask
+# --------------------------------- [edit] ---------------------------------- #
+set FLASK_APP=pybo
+set FLASK_ENV=development
+# --------------------------------------------------------------------------- #
+C:/dev/works/python_web_framework/flask_works/venv/Scripts/activate
